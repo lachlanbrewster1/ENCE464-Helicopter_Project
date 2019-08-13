c00faffed09943ce5b33ad4d5bd04e3cadf8665f
Greg Bates <gba52@canterbury.ac.nz>
2019-08-13 20:08:21 +1200

    PWM input configuration and duty cycle calculation. Run into error #10010 but confident written code is correct.

--- helirig_emulator/mainProg.c
+++ helirig_emulator/mainProg.c
@@ -10,9 +10,10 @@
 //
 //*****************************************************************************
 
-
+#include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include "../inc/hw_memmap.h" // Macros for the different ports and peripherals defined here.
 #include "../driverlib/pin_map.h" // Definitions of pin addresses
 #include "../driverlib/adc.h" //         }
@@ -24,14 +25,48 @@
 #include "../initialisers.h"
 #include "../sharedConstantsTypes.h"
 #include "mainProg.h"
+#include "../inc/tm4c123gh6pm.h"
+#include "../inc/hw_types.h"
+#include "../driverlib/timer.h"
 
+
+#include "../inc/hw_ints.h"
+#include "../driverlib/pwm.h"
+#include "../driverlib/debug.h"
+#include "../utils/ustdlib.h"
+#include "../driverlib/uart.h"
+
+
+
+//Input Edge Time Capture include and defines
+volatile double duty = 0, start = 0, stop = 0;
+#define PWM_Frequency 250 //Hz
+#define PWM_DIVIDER_CODE     SYSCTL_PWMDIV_4
+#define SAMPLE_RATE_HZ     250
+#define SYSTICK_RATE_HZ    100    // SysTick interrupt config.
+static uint32_t g_ulSampCnt;
+
+
+
+//DAC related includes
 #define DAC_WRITE_CMD_NGA_NSHDN_2048 0x3800
 #define DAC_CMD_ARRAY_LENGTH 4
 static const uint32_t dacWriteCmdValue = 0x00003800; // First four zeros are redundant
 #define DAC_CMD_LENGTH_W_RDDT_BITS 16
 
-#define sampling_rate = 10              //In milliseconds
+
+//Helicopter model related includes and globals
 volatile double current_height = 0;     //Current height of the helicopter initialised to zero
+
+
+//*****************************************************************************
+// Local prototypes
+//*****************************************************************************
+void initClock (void);
+void SysTickIntHandler(void);
+void helicopterHeight (void);
+void initTimer (void);
+void dutyCycle (void);
 
 // ----------------------------------------------------------------------------
 // Start of interrupt handler definitions
@@ -126,6 +161,34 @@ volatile double current_height = 0;     //Current height of the helicopter initi
 //    }
 //}
 
+//*****************************************************************************
+// Initialisation functions for the clock (incl. SysTick), ADC, display
+//*****************************************************************************
+void
+initClock (void)
+{
+    // Set the clock rate to 20 MHz
+    SysCtlClockSet (SYSCTL_SYSDIV_10 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN |
+                   SYSCTL_XTAL_16MHZ);
+    // Set up the period for the SysTick timer
+    SysTickPeriodSet(SysCtlClockGet() / SAMPLE_RATE_HZ);
+    SysTickIntRegister(SysTickIntHandler);
+    SysCtlPWMClockSet(PWM_DIVIDER_CODE);
+    SysTickIntEnable();
+    SysTickEnable();
+}
+
+//*****************************************************************************
+// Interrupt handler for the SysTick interrupt.
+//*****************************************************************************
+void
+SysTickIntHandler(void)
+{
+    // Initiate a conversion
+    ADCProcessorTrigger(ADC0_BASE, 3);
+    g_ulSampCnt++;
+}
+
 
 //*****************************************************************************
 // This is attempt 1 to model the behaviour of the helicopter
@@ -158,26 +221,24 @@ helicopterHeight (double main_thrust)
 // This is attempt 2 to model the behaviour of the helicopter
 //*****************************************************************************
 void
-helicopterHeight (double main_thrust)
+helicopterHeight (void)
 {
     //Force due to weight is a changing quantity which is influenced from the current height of the helicopter.
     double weight_force = 0.2 + current_height/100;
 
     //Effective thrust is the force of the motor minus the weight force
-    double thrust = main_thrust - (weight_force * 100);
+    double thrust = duty - (weight_force * 100);
 
     //Acceleration is dependent upon the effective thrust of the motor
-    double acceleration = ((2*thrust)*(2*thrust)/200);
+    double acceleration = ((thrust*thrust)/400);
 
     //Check the direction
     if (thrust < 0) {
         acceleration = -acceleration;
     }
 
-    //change in height is the effecitve thrust multiplied byt the sampling rate
-    double change_height = acceleration * sampling_rate;
-
-    current_height += change_height;
+    //change in height is the effective thrust multiplied by the sampling rate
+    current_height += acceleration * SAMPLE_RATE_HZ; //This is the change in height calculation
 
     if (current_height < 0) {
         current_height = 0;
@@ -188,8 +249,46 @@ helicopterHeight (double main_thrust)
     }
 }
 
+//*****************************************************************************
+// Function to initalise all the required features to measure duty cycle of input PWM signal
+//*****************************************************************************
+void
+initTimer(void)
+{
+    //Calls to initialise Interrupts and PWM reads
+    GPIOIntRegister (GPIO_PORTC_BASE, dutyCycle);
+    GPIOPinTypeGPIOInput (GPIO_PORTC_BASE, GPIO_PIN_4);
+    GPIOIntTypeSet (GPIO_PORTC_BASE, GPIO_INT_PIN_4, GPIO_BOTH_EDGES);
+    GPIOIntEnable(GPIO_PORTC_BASE, GPIO_INT_PIN_4);
+
+    //Initalise Timer
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+    TimerConfigure(TIMER0_BASE, TIMER_CFG_A_CAP_TIME);
 
 
+
+}
+
+//*****************************************************************************
+// Function calculate the duty cycle of the input PWM signal
+//*****************************************************************************
+void
+dutyCycle(void)
+{
+    //When negative edge is hit, record the values and find the difference
+    int32_t state = GPIOPinRead(GPIO_PORTC_BASE, GPIO_PIN_4);
+
+    //If the signal is a leading edge
+    if (state == 1) {
+        start = TimerValueGet(TIMER0_BASE, TIMER_A);
+    } else { //If the signal is falling edge
+        stop = TimerValueGet(TIMER0_BASE, TIMER_A);
+        //Only want to calculate on the falling edge or else duty will go from positive to negative.
+        duty = (stop - start)*PWM_Frequency*100;
+    }
+
+    GPIOIntClear(GPIO_PORTC_BASE, GPIO_INT_PIN_4);
+}
 
 int
 main (void)
@@ -218,6 +317,12 @@ main (void)
     uint8_t ui8Idx;
     char *pcChars = "DACA";
     // Loop and send this string infinitely
+
+    //Configure to receive input PWM signals
+    initTimer();
+
+    TimerEnable(TIMER0_BASE, TIMER_BOTH);
+
     while(true)
     {
         for (ui8Idx = 0; ui8Idx < DAC_CMD_ARRAY_LENGTH; ui8Idx++)
@@ -225,7 +330,7 @@ main (void)
             SSIDataPut (SSI0_BASE, pcChars[ui8Idx]);
         }
 
-        helicopterHeight (double main_thrust)
+        helicopterHeight ();
     }
 }
 
